fa4d4bb4a2b792250af7270ccc0779fdb3b1cb3b
Shahzad Tahir <shahzadtahirch@gmail.com>
2022-11-11 16:18:04 +0500

    Utilizing js packages with vite!

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,5 @@
 import './bootstrap';
+
+import { formatDistance, subDays } from 'date-fns'
+
+alert(formatDistance(subDays(new Date(), 3), new Date(), { addSuffix: true }));
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -10,7 +10,7 @@
         <link href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
 
         <!-- Styles -->
-        @vite(['resources/css/app.scss', 'resources/js/app.js'])
+        @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
     <body>
         <h1>Hello, <span>SCSS!</span></h1>
